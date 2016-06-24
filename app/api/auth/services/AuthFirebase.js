51d2ef3a84d440c405ab4cc74eee2acd2e2b2bd7
amrit sahoo <amritsahoo@gmail.com>
2016-06-24 11:16:20 +0530

    testing alert

--- app/api/auth/services/AuthFirebase.js
+++ app/api/auth/services/AuthFirebase.js
@@ -161,7 +161,7 @@ export default class AuthFirebase {
         }
       });
 
-      xhr.open("POST", "http://eduforum-eduscape.rhcloud.com/api/token");
+      xhr.open("POST", "http://educron.com/api/token");
       xhr.setRequestHeader("content-type", "application/json");
       xhr.setRequestHeader("cache-control", "no-cache");
 
